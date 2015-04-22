3c78791604f8eb6f41092dabd83c292c5936ff0c
Alexander Skafte <alexanderskafte@gmail.com>
2015-04-22 19:29:45 +0200

    Fix indentation

--- BicycleGaragePro/src/bicyclegarage/Bicycle.java
+++ BicycleGaragePro/src/bicyclegarage/Bicycle.java
@@ -1,42 +1,45 @@
 package bicyclegarage;
 
 public class Bicycle {
-	private final String ID; // 5-digit ID number.
-	private final User owner;
+    private final String ID; // 5-digit ID number.
+    private final User owner;
 
-	// Add these eventually
-//	private Date lastCheckIn;
-//	private boolean isCheckedIn;
+    // Add these eventually
+    // private Date lastCheckIn;
+    // private boolean isCheckedIn;
 
-	/**
-	 * Creates a new Bicycle.
-	 * Bicycle ID and owner can't be changed after initialization.
-	 * @param bicycleID	The 5-digit identification number of the bicycle
-	 * @param owner		The bicycle's owner
-	 */
-	public Bicycle(String bicycleID, User owner) {
-		ID = bicycleID;
-		this.owner = owner;
-//		isCheckedIn = false;
-	}
-	
-	public String getID() {
-		return ID;
-	}
-	
-	public User getOwner() {
-		return owner;
-	}
-	
-	// Check in and check out should be handled by the manager, right?
-	
-//	void checkIn(Date date) {
-//		
-//		isCheckedIn = true;
-//	}
-//	
-//	void checkOut(Date date) {
-//		
-//		isCheckedIn = false;
-//	}
+    /**
+     * Creates a new Bicycle. Bicycle ID and owner can't be changed after
+     * initialization.
+     * 
+     * @param bicycleID
+     *            The 5-digit identification number of the bicycle
+     * @param owner
+     *            The bicycle's owner
+     */
+    public Bicycle(String bicycleID, User owner) {
+        ID = bicycleID;
+        this.owner = owner;
+        // isCheckedIn = false;
+    }
+
+    public String getID() {
+        return ID;
+    }
+
+    public User getOwner() {
+        return owner;
+    }
+
+    // Check in and check out should be handled by the manager, right?
+
+    // void checkIn(Date date) {
+    //
+    // isCheckedIn = true;
+    // }
+    //
+    // void checkOut(Date date) {
+    //
+    // isCheckedIn = false;
+    // }
 }
--- BicycleGaragePro/src/bicyclegarage/BicycleGarageManager.java
+++ BicycleGaragePro/src/bicyclegarage/BicycleGarageManager.java
@@ -7,39 +7,39 @@ import interfaces.PinCodeTerminal;
 import java.util.Set;
 
 public class BicycleGarageManager {
-	
-	private Set<User> users;
-	
-	public boolean addUser(User user) {
-		return users.add(user);
-	}
-	
-	public void listAllUsers() {
-		for (User u : users) {
-			System.out.println(u.toString());
-		}
-	}
 
-	public void entryBarcode(String code) {
-		// TODO Auto-generated method stub
-		
-	}
+    private Set<User> users;
 
-	public void exitBarcode(String code) {
-		// TODO Auto-generated method stub
-		
-	}
+    public boolean addUser(User user) {
+        return users.add(user);
+    }
 
-	public void entryCharacter(char c) {
-		// TODO Auto-generated method stub
-		
-	}
+    public void listAllUsers() {
+        for (User u : users) {
+            System.out.println(u.toString());
+        }
+    }
 
-	public void registerHardwareDrivers(BarcodePrinter printer,
-			ElectronicLock entryLock, ElectronicLock exitLock,
-			PinCodeTerminal terminal) {
-		// TODO Auto-generated method stub
-		
-	}
+    public void entryBarcode(String code) {
+        // TODO Auto-generated method stub
+
+    }
+
+    public void exitBarcode(String code) {
+        // TODO Auto-generated method stub
+
+    }
+
+    public void entryCharacter(char c) {
+        // TODO Auto-generated method stub
+
+    }
+
+    public void registerHardwareDrivers(BarcodePrinter printer,
+            ElectronicLock entryLock, ElectronicLock exitLock,
+            PinCodeTerminal terminal) {
+        // TODO Auto-generated method stub
+
+    }
 
 }
