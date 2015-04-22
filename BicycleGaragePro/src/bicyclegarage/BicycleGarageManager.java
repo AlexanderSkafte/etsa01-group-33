2f6901044e41f72a0b3517b3d4dd53901aa12e49
Alexander Skafte <alexanderskafte@gmail.com>
2015-04-22 16:38:30 +0200

    Add interfaces and test classes.

---
+++ BicycleGaragePro/src/bicyclegarage/BicycleGarageManager.java
@@ -0,0 +1,31 @@
+package bicyclegarage;
+
+import interfaces.BarcodePrinter;
+import interfaces.ElectronicLock;
+import interfaces.PinCodeTerminal;
+
+public class BicycleGarageManager {
+
+	public void entryBarcode(String code) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void exitBarcode(String code) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void entryCharacter(char c) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void registerHardwareDrivers(BarcodePrinter printer,
+			ElectronicLock entryLock, ElectronicLock exitLock,
+			PinCodeTerminal terminal) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
